4ddc3f6e5a43cf413b7ca45a07ae63d004eb9c2e
Angel Gabriel Larios Acosta <lariosacostaa@gmail.com>
2022-10-09 14:54:02 -0500

    se agrego el archivo 3-app.js

---
+++ 03-app.js
@@ -0,0 +1,9 @@
+console.log('I want pizza'[0])
+/* 
+devuelve I
+*/
+
+/* 
+Explicacion
+Las cadenas de texto en javascript, son iterables, por ello, al igual que con los arreglos, es posible acceder a sus caracteres individuales con la notacion de corchetes
+*/
